fc2aad5a4ce4cd0d8303ede9db2316689c93c175
CleviaDiany <clevia.diany30@sma.belajar.id>
2024-09-15 10:15:49 +0700

    Add files via upload

---
+++ latihan1.cpp
@@ -0,0 +1,16 @@
+//Dibuat oleh clevia
+//Tanggal dibuat dua september
+
+#include <iostream>
+
+using namespace std; //perintah untuk membuat variabel std
+
+int main() 
+{
+    cout << "STMIK Penusa" << endl;
+    cout << "Algoritma Pemrograman" << endl;
+    cout << 5 << endl;
+    cout << 'A' << endl;
+
+    return 0;
+}
