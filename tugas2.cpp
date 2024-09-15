92a9a2a983151b340d1b3ac637da622edadc55a3
CleviaDiany <clevia.diany30@sma.belajar.id>
2024-09-15 16:33:49 +0700

    Add files via upload

---
+++ tugas2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	float jumlahpenjualan,lebihdari=500000,komisi=0.1;
+	
+    cout << "Masukkan Jumlah Penjualan : ";
+    cin >> jumlahpenjualan;
+    
+    if (jumlahpenjualan<=500000) {
+        komisi=jumlahpenjualan*0.1;
+    }else {
+        komisi=500000*0.1+((jumlahpenjualan-500000)*0.15);
+    }
+    
+    cout << "Total Komisi Yang Didapat Adalah : Rp. " << komisi << endl;   
+    
+    return 0;
+}         
+    
+    
