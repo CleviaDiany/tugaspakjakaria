2907eacad73e9ec983902ab4e32b8bba37339f2b
CleviaDiany <clevia.diany30@sma.belajar.id>
2024-09-15 10:25:25 +0700

    Add files via upload

---
+++ latihan3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int luas,panjang,lebar;
+    cout << "Masukkan Nilai Panjang = " << endl; 
+    cin >> panjang;
+    cout << "Masukkan Nilai Lebar = " << endl;
+    cin >> lebar;
+    luas=panjang*lebar;
+    cout << "Luas Persegi Panjang = " << luas << endl;
+
+    return 0;
+}
