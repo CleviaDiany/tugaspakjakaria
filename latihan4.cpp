a75c7caaa089e338cb0f50e4f94960103fd6b721
CleviaDiany <clevia.diany30@sma.belajar.id>
2024-09-15 11:07:34 +0700

    latihan4.cpp

--- latihan4.cpp
+++ latihan4.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main() 
 {
     float alas,tinggi,luas;
-    cout << "Masukkan Nilai Alas = " << endl; 
+    cout << "Masukkan Nilai Alas = "; 
     cin >> alas;
-    cout << "Masukkan Nilai Tinggi = " << endl;
+    cout << "Masukkan Nilai Tinggi = ";
     cin >> tinggi;
-    luas=0,5*alas*tinggi;
-    cout << "Luas Segitiga Siku-siku = " << luas << endl;
+    luas=alas*tinggi*0.5;
+    cout << "Luas Segitiga Sama Kaki = " << luas << endl;
 
     return 0;
 }
