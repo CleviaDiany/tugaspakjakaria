cf46cf41aab9de248c191721e2090c22c0841db1
CleviaDiany <clevia.diany30@sma.belajar.id>
2024-10-03 14:45:46 +0700

    Add files via upload

---
+++ latihan15.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i = 0;
+    while (i < 5) {
+    int j = 0;
+    while (j < 5) {
+         cout << "Perulangan ke-" << i << "," << j << endl;
+         i++;
+         j++;
+        }
+    }    
+    return 0;
+}
