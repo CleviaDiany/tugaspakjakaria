f7977de6805dca7bc7ab37197f8169804b4d3c4b
CleviaDiany <clevia.diany30@sma.belajar.id>
2024-10-10 19:53:07 +0700

    Add files via upload

---
+++ latihan21.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int nilai[10], total = 0;
+	float rataRata;
+	// Input nilai 
+	for (int i = 0; i < 10; i++) {
+		cout << "Masukkan nilai ke-" << i + 1 << ": ";
+		cin >> nilai [i];
+		total += nilai[i]; // Menghitung total
+		//total = total + nilai[i]
+	}
+	// Menghitung rata-rata
+	rataRata = total / 10.0;
+	// Menampilkan hasil
+	cout << "Total nilai: " << total << endl;
+	cout << "Rata-rata nilai: " << rataRata << endl;
+	return 0;
+}
