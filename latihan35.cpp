5be2a2885ef14a78c2d3fface0aa79ba81fa4141
Clevia Diany (240121088) <clevia.diany30@sma.belajar.id>
2024-11-15 11:28:48 +0700

    Add files via upload

---
+++ latihan35.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+float hitung ( int s );
+int main()
+{
+	float volume, sisi;
+	cout << "\n Menghitung Volume KUBUS ";
+	cout << "\n Sisi KUBUS : ";
+	cin >> sisi;
+	volume = hitung(sisi);
+	cout << "\n Volume KUBUS yaitu : " << volume;
+}
+
+float hitung ( int s )
+{
+	return ( s * s * s);
+	
+}
